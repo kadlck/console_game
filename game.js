d34da9f364013808f00543014c8e608db7c09db6
Dominika <dom.kadleckova@gmail.com>
2024-11-09 13:32:17 +0100

    added code for the first part

--- game.js
+++ game.js
@@ -90,52 +90,46 @@ function playGame(rounds=0){
     }
 }
 
-async function wrap_around (){
-    function getClick(){
-        const first = document.querySelector(".first");
-        const startBtn = document.querySelector(".start");
-
-        first.removeChild(startBtn);
-
-        const second = document.querySelector(".second");
-        const firstBtns1 = document.createElement("button");
-        firstBtns1.classList.add("yes");
-        firstBtns1.textContent = "YES";
-        firstBtns1.addEventListener("click", changeVariable);
-        const firstBtns2 = document.createElement("button");
-        firstBtns2.classList.add("no");
-        firstBtns2.textContent = "NO";
-        firstBtns2.addEventListener("click", changeVariable);
-
-        second.appendChild(firstBtns1)
-        second.appendChild(firstBtns2)
-    }
-    
-    console.log("before getClick")
-    // await getClick()
-    console.log("after getClick")
-    
+function wrap_around (decision){
     if (decision == "YES"){
         humanScore = 0
         computerScore = 0
         playGame()
-        wrap_around()
+        getSecondClick()
+    }else{
+        const second = document.querySelector(".second");        
+        second.replaceChildren();
+        const secondsecondtext = document.createElement("p");
+        secondsecondtext.textContent = "Yeah. No. Click YES"
+        second.appendChild(secondsecondtext);
+        getSecondClick();
     }
-    else if (decision == "NO") {
-        console.log("What now?")
-    }
-}
-
-function changeVariable(event){
-    let decision = event.target.textContent;
 }
 
 let humanScore = 0
 let computerScore = 0
 let decision = "Problem"
+
 const first = document.querySelector(".first");
 const startBtn = document.createElement("button");
 startBtn.classList.add("start");
 startBtn.textContent = "Click me baby!";
-startBtn.addEventListener("click", wrap_around);
+startBtn.addEventListener("click", function together(){
+    first.removeChild(startBtn);
+    getSecondClick();
+});
 first.appendChild(startBtn);
+
+function getSecondClick(){
+    const second = document.querySelector(".second");
+    const firstBtns1 = document.createElement("button");
+    firstBtns1.textContent = "YES";
+    firstBtns1.addEventListener("click", function exectute(){
+        wrap_around("YES")});
+    const firstBtns2 = document.createElement("button");
+    firstBtns2.textContent = "NO";
+    firstBtns2.addEventListener("click", function exectute(){
+        wrap_around("NO")});
+    second.appendChild(firstBtns1)
+    second.appendChild(firstBtns2)
+}
