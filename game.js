536711f80edd5b1ffafd29af93aff98587222f5e
Dominika <dom.kadleckova@gmail.com>
2024-11-09 17:55:56 +0100

    Finished the UI

--- game.js
+++ game.js
@@ -2,91 +2,86 @@ function getComputerChoice() {
     const a = Math.floor(Math.random() * 3);
     let decision = "Problem";
     if (a == 0){
-        decision = "rock"
+        decision = "ROCK"
     }
     else if(a == 1){
-        decision = "paper"
+        decision = "PAPER"
     }
     else if (a == 2){
-        decision = "scissors"
+        decision = "SCISSORS"
     }
     return decision    
 };
 
-function getHumanChoice(){
-    let possibilities = ["rock", "paper", "scissors"];
-    let choice = prompt("Your choice?").toLowerCase();
-    if (possibilities.includes(choice)){
-        return choice
-    }
-    else {
-        return "Problem"
-    }
-
-};
-
-function playRound(){
-    const humanSelection = getHumanChoice();
+function playRound(click){
+    const humanSelection = click;
     const computerSelection = getComputerChoice();
-    console.log("Computer chose: " + computerSelection);
-    console.log("You chose: " + humanSelection);
+    printText(("Computer chose: " + computerSelection));
+    printText(("You chose: " + humanSelection));
 
     if (humanSelection == computerSelection){
-        console.log("It's a draw");
-        computerScore += 1
-        humanScore += 1
+        printText("It's a draw");
+        computerScore += 1;
+        humanScore += 1;
     }
     else if(humanSelection != computerSelection){
         // rock beats scissors
         // scissors beats paper
         // paper beats rock
-        if (humanSelection == "rock"){
-            if (computerSelection == "scissors"){
-                console.log("You win this round")
-                humanScore += 1
+        if (humanSelection == "ROCK"){
+            if (computerSelection == "SCISSORS"){
+                printText("You win this round");
+                humanScore += 1;
             }
             else {
-                console.log("You loose this round")
-                computerScore += 1
+                printText("You loose this round");
+                computerScore += 1;
             }
         }
-        else if (humanSelection == "scissors"){
-            if (computerSelection == "paper"){
-                console.log("You win this round")
-                humanScore += 1
+        else if (humanSelection == "SCISSORS"){
+            if (computerSelection == "PAPER"){
+                printText("You win this round");
+                humanScore += 1;
             }
             else {
-                console.log("You loose this round")
-                computerScore += 1
+                printText("You loose this round");
+                computerScore += 1;
             }
         }
-        else if (humanSelection == "paper"){
-            if (computerSelection == "rock"){
-                console.log("You win this round")
-                humanScore += 1
+        else if (humanSelection == "PAPER"){
+            if (computerSelection == "ROCK"){
+                printText("You win this round");
+                humanScore += 1;
             }
             else {
-                console.log("You loose this round")
-                computerScore += 1
+                printText("You loose this round");
+                computerScore += 1;
             }
-    
         }
     }
-    else{console.log("Problem")}
+    resume()
 };
 
-function playGame(rounds=0){
-    let maxRounds=4
-    if (rounds <= maxRounds){
-        console.log("Round: " + rounds)
-        console.log("Score: "+ computerScore+" for computer " + humanScore + " for you.")
-        playRound()
-        console.log("Score: "+ computerScore+" for computer " + humanScore + " for you.")
+function resume(){}
+
+async function playGame(rounds=0, maxRounds=4){
+    if (rounds < maxRounds ) {
+        var promise = new Promise((resolve) => {
+            resume = () => resolve()
+          });
+        await promise
+        printText(("Round " + rounds + " is " + computerScore + " for computer " + humanScore + " for you."));
         rounds += 1
         playGame(rounds)
     }
-    else{
-        console.log("The final score is: " + humanScore + " for you, and: "+ computerScore + " for computer.")
+    else if (rounds == maxRounds){
+        const second = document.querySelector(".second");        
+        //second.replaceChildren();
+        const finalMessage = document.createElement("p");
+        finalMessage.textContent = "The final score is: " + humanScore +
+         " for you, and: "+ computerScore + " for computer.";
+        second.appendChild(finalMessage)
+        getSecondClick()
     }
 }
 
@@ -95,7 +90,7 @@ function wrap_around (decision){
         humanScore = 0
         computerScore = 0
         playGame()
-        getSecondClick()
+        new_buttons()
     }else{
         const second = document.querySelector(".second");        
         second.replaceChildren();
@@ -109,7 +104,6 @@ function wrap_around (decision){
 let humanScore = 0
 let computerScore = 0
 let decision = "Problem"
-
 const first = document.querySelector(".first");
 const startBtn = document.createElement("button");
 startBtn.classList.add("start");
@@ -122,6 +116,8 @@ first.appendChild(startBtn);
 
 function getSecondClick(){
     const second = document.querySelector(".second");
+    const quest = document.createElement("p");
+    quest.textContent = "Do you want to play?"
     const firstBtns1 = document.createElement("button");
     firstBtns1.textContent = "YES";
     firstBtns1.addEventListener("click", function exectute(){
@@ -130,6 +126,39 @@ function getSecondClick(){
     firstBtns2.textContent = "NO";
     firstBtns2.addEventListener("click", function exectute(){
         wrap_around("NO")});
+    second.appendChild(quest)
     second.appendChild(firstBtns1)
     second.appendChild(firstBtns2)
+}
+
+function printText(text){
+    const second = document.querySelector(".second");
+    const textIwant = document.createElement("p")
+    textIwant.textContent = text
+    second.appendChild(textIwant)
+}
+
+function new_buttons(){
+    const second = document.querySelector(".second");
+    second.replaceChildren();
+    generateButtons()
+}
+
+function generateButtons(){
+    const second = document.querySelector(".second");
+    const rock = document.createElement("button");
+    rock.textContent = "ROCK";
+    rock.addEventListener("click", function exectute(){
+        playRound("ROCK")});
+    const paper = document.createElement("button");
+    paper.textContent = "PAPER";
+    paper.addEventListener("click", function exectute(){
+    playRound("PAPER")});
+    const scissors = document.createElement("button");
+    scissors.textContent = "SCISSORS";
+    scissors.addEventListener("click", function exectute(){
+        playRound("SCISSORS")});
+    second.appendChild(rock)
+    second.appendChild(paper)
+    second.appendChild(scissors)
 }
